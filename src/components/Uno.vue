af73494bf6421407d8b09af6b6985a525fdc521d
unknown <daniel>
2021-09-06 22:50:41 -0500

    sond commit

---
+++ src/components/Uno.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <h1>Juego 1</h1>
+    <!--<img src="../assets/uno.jpg" alt="hola">-->
+
+    <img :src="imagen" alt="Imagen juego">
+
+    <h4>{{Nombre}}</h4>
+    <h4>{{Valor}}</h4>
+
+</div>
+
+  
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Uno',
+  props:{
+      imagen:{
+          type: String,
+      },
+      Nombre:{
+          type: String,
+      },
+      Valor:{
+          type: Number,
+      }
+  }
+}
+</script>
---
+++ src/views/Principal.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <h1>Tienda online</h1>
+
+    <img src="../assets/Principal.jpg" alt="hola">
+
+    <uno :Nombre="'Fifa 2021'" :Valor="15000" :imagen=" 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Iyc5PA9NTv8p6U0n3IryzWwS2ueQXzyFMQ&usqp=CAU '"></uno>
+
+
+</div>
+
+  
+</template>
+
+<script>
+import Uno from '../components/Uno.vue'
+
+
+
+export default {
+  name: 'Principal',
+  components: {
+    Uno
+  }
+}
+
+</script>
